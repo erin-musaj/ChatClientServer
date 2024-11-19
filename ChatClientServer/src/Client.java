9a907d3afad0c256d7294c5ac15abdaa33bb58ff
erin-musaj <erin.musaj@buonarroti.tn.it>
2024-11-19 19:01:41 +0100

    final commit 2

--- ChatClientServer/src/Client.java
+++ ChatClientServer/src/Client.java
@@ -3,11 +3,12 @@ import java.net.*;
 public class Client {
     public static void main(String[] args) {
         String serverAddress = "localhost";
-        int port = 1234;
-        try (Socket socket = new Socket(serverAddress, port)) {
+        int port = 6969;
+        try (Socket s = new Socket(serverAddress, port)) {
             System.out.println("Connesso al server");
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
             BufferedReader ui = new BufferedReader(new InputStreamReader(System.in));
 
             String line = "";
@@ -20,9 +21,11 @@ public class Client {
                 out.println(line);
             }
 
-            System.out.println("Connessione con il Server chiusa.");
             in.close();
             out.close();
+            s.close();
+
+            System.out.println("Connessione con il Server chiusa.");
 
         } catch (IOException e) {
             System.err.println("Errore nella comunicazione: " + e.getMessage());
--- ChatClientServer/src/Server.java
+++ ChatClientServer/src/Server.java
@@ -2,30 +2,30 @@ import java.io.*;
 import java.net.*;
 public class Server {
     public static void main(String[] args) {
-        int port = 1234;
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
+        int port = 6969;
+        try (ServerSocket ss = new ServerSocket(port)) {
             System.out.println("Server in ascolto sulla porta " + port);
-            Socket clientSocket = serverSocket.accept();
+
+            Socket cs = ss.accept();
             System.out.println("Connessione stabilita con il client");
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+            PrintWriter out = new PrintWriter(cs.getOutputStream(), true);
             BufferedReader ui = new BufferedReader(new InputStreamReader(System.in));
 
-
-            String line = "";
             String response = "";
             while (!response.equals("FINE") && !response.equals(null)) {
-                line = ui.readLine();
-                out.println(line);
+
+                out.println(ui.readLine());
 
                 response = in.readLine();
                 System.out.println("Echo dal Client: " + response);
             }
 
-            clientSocket.close();
+            cs.close();
             in.close();
             out.close();
-            System.out.println("Connessione con il client chiusa.");
+            System.out.println("Connessione con il Client chiusa.");
 
         } catch (IOException e) {
             System.err.println("Errore nella comunicazione: " + e.getMessage());
